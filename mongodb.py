87877e55cf913de0c7955ecee99983f2a783cc14
Tommy Lariviere <101297711+tomlol128@users.noreply.github.com>
2025-10-06 08:32:52 -0400

    Create mongodb.py

---
+++ mongodb.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from pymongo import MongoClient
+
+
+# Lire tous les CSV avec le bon séparateur
+df_all = pd.read_csv("sherbrooke_summer_hourly_weather2025.csv", sep=',', encoding='utf-8-sig')
+
+
+# Nettoyer les colonnes si nécessaire (exemple : remplacer 'M' ou 'LegendMM' par None)
+df_all.replace(['M', 'LegendMM', ''], None, inplace=True)
+
+# Convertir en dictionnaires
+data = df_all.to_dict(orient='records')
+
+# Connexion à MongoDB Atlas
+client = MongoClient("mongodb+srv://anwar:mimimomo01@projet2.vcv1jsv.mongodb.net/")
+db = client["meteo_db"]
+collection = db["sherbrooke"]
+
+# Insérer toutes les données
+collection.insert_many(data)
+
+print(f"{len(data)} documents importés dans MongoDB !")
+
+
